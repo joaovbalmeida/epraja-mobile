084049bdd96ff6ab8e323d61c02f7110a621a8be
joaovbalmeida <joaovbalmeida@gmail.com>
2017-10-25 23:38:22 -0200

    Basic Redux Added

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/layouts/login.js'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+import sessionReducer from './src/store/reducers/reducer.session'
+
+let store = createStore(sessionReducer)
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Login />
-    );
-  }
+	constructor(props){
+		super(props)
+		alert(store.getState());
+	}
+	
+  	render() {
+		return (
+			<Provider store={store}>
+				<Login />
+			</Provider>
+		);
+	}
 }
---
+++ src/store/reducers/reducer.session.js
@@ -0,0 +1,10 @@
+const sessionReducer = (state = 0, action) => {
+	switch (action.type){
+		case 'UPDATE_TABLE_NUMBER':
+			return action.number
+		default:
+			return state
+	}
+}
+
+export default sessionReducer
