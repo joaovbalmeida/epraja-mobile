d885300c07a6dd25a0ef475533b1ed3776a54a2b
joaovbalmeida <joaovbalmeida@gmail.com>
2017-12-07 19:04:15 -0200

    tw

--- App.js
+++ App.js
@@ -9,67 +9,43 @@ import { PersistGate } from 'redux-persist/es/integration/react';
 import MainNav, { BypassCheckinNav } from './src/layouts/main';
 import sessionReducer from './src/store/reducers/reducer.session';
 
+const config = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const reducer = persistCombineReducers(config, { sessionReducer });
+
+const middlewares = [];
+
+middlewares.push(thunk);
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(createLogger({
+    collapsed: false,
+  }));
+}
+
 console.ignoredYellowBox = ['Setting a timer', 'Warning: Each child in'];
 
-function configureStore() {
-
-  const middlewares = [];
-  if (process.env.NODE_ENV === 'development') {
-    middlewares.push(createLogger({
-      collapsed: false,
-    }));
-    middlewares.push(thunk);
-  }
-  const reducer = persistCombineReducers(config, { sessionReducer });
-  const config = {
-    key: 'root',
-    storage: AsyncStorage,
-  };
-
-  const store = createStore(reducer, compose(applyMiddleware(...middlewares)));
-  const persistor = persistStore(store);
-
-  const state = store.getState()
-  const route = [];
-
-  if (state.sessionReducer.tableNumber !== 0) {
-    route.push(<BypassCheckinNa/>)
-    return {store, persistor, route};
-  } else {
-    route.push(<MainNav/>)
-    return {store, persistor, route};
-  }
+const store = createStore(reducer, compose(applyMiddleware(...middlewares)));
+const persistor = persistStore(store);
+const route = [];
+const state = store.getState()
+if (state.sessionReducer.tableNumber !== 0) {
+  route.push(<BypassCheckinNav />);
+} else {
+  route.push(<MainNav />)
 }
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      store: null,
-      persistor: null,
-      route: null,
-    }
-  }
-
-  async componentWillMount () {
-    const {store, persistor, route} = await configureStore();
-    this.setState({
-      store: store,
-      persistor: persistor,
-      route: route,
-    });
-  }
-
-  render {
-    return (
-      <Provider store={this.state.store}>
-        <PersistGate persistor={this.state.persistor}>
-          {this.state.route}
-        </PersistGate>
-      </Provider>
-    )
-  }
-}
+console.log(state);
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        {route}
+      </PersistGate>
+    </Provider>
+  );
+};
 
 export default App;
-
